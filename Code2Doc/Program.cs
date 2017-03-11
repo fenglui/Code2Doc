836cbd9be13b7024602ca48a7784e3fc2ba2c666
fenglui <3090641@qq.com>
2017-03-11 12:03:31 +0800

    v1

---
+++ Code2Doc/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code2Doc
+{
+    class Program
+    {
+        static List<string> FindFile2(string sSourcePath)
+        {
+            List<String> list = new List<string>();
+            //遍历文件夹
+            DirectoryInfo theFolder = new DirectoryInfo(sSourcePath);
+            FileInfo[] thefileInfo = theFolder.GetFiles("*.*", SearchOption.TopDirectoryOnly);
+            foreach (FileInfo NextFile in thefileInfo)  //遍历文件
+                list.Add(NextFile.FullName);
+            //遍历子文件夹
+            DirectoryInfo[] dirInfo = theFolder.GetDirectories();
+            foreach (DirectoryInfo NextFolder in dirInfo)
+            {
+                //list.Add(NextFolder.ToString());
+                FileInfo[] fileInfo = NextFolder.GetFiles("*.*", SearchOption.AllDirectories);
+                foreach (FileInfo NextFile in fileInfo)  //遍历文件
+                    list.Add(NextFile.FullName);
+            }
+            return list;
+        }
+
+        static string getSrcPath(string[] args)
+        {
+            var srcPath = String.Empty;
+            if (args.Length > 0)
+            {
+                srcPath = args[0];
+            }
+
+            if (Directory.Exists(srcPath) == false)
+            {
+                Console.WriteLine("src path not exists");
+                Console.WriteLine("pls type or paste your src path:");
+
+                return getSrcPath(new string[] { Console.ReadLine() });
+            }
+
+            return srcPath;
+        }
+
+        static void Main(string[] args)
+        {
+            string dir = AppDomain.CurrentDomain.BaseDirectory;
+            string TemPath = System.IO.Path.Combine(dir, "test.docx");
+
+            var srcPath = getSrcPath(args);
+
+            var files = FindFile2(srcPath);
+
+            foreach (var file in files)
+            {
+                Console.WriteLine(file);
+
+                var fileName = System.IO.Path.GetFileName(file);
+
+                Console.WriteLine(fileName);
+
+                var fileType = System.IO.Path.GetExtension(file);
+
+                Console.WriteLine(fileType);
+
+                if (fileType == ".png" || fileType == ".jpg" || fileType == ".gif" || fileType == ".docx" || fileType == ".css")
+                    continue;
+
+                var fileDir = System.IO.Path.GetDirectoryName(file);
+                Console.WriteLine(fileDir);
+
+                var docFile = System.IO.Path.Combine(fileDir, fileName + ".docx");
+
+                System.IO.File.Copy(TemPath, docFile, true);
+
+
+                Report report = new Report();
+                report.CreateNewDocument(docFile); //模板路径
+
+                string text = File.ReadAllText(file);
+
+                Console.Write(text.Length);
+
+                report.InsertText("code", text);
+
+                report.Document.Close();
+
+                //try
+                //{
+                //    report.SaveDocument(docFile); //文档路径
+                //}
+                //catch (Exception)
+                //{
+
+                //} 
+            }
+
+
+            Console.WriteLine("OK");
+
+            //report.SaveDocument(RepPath); //文档路径
+
+        }
+    }
+}
---
+++ Code2Doc/README.md
@@ -0,0 +1,45 @@
+﻿# Code2Doc
+
+## 将代码转换为word doc格式
+
+** code2doc是一个在windows平台将目标文件夹中任意文件转换为word doc格式的命令行程序。
+** 它会跳过这些类型的文件：png,jpg,gif,docx,css
+
+**Demo地址：暂无 **
+
+
+
+**更新日志**
+>**Vision 1**
+>
+>1.基础功能。
+>
+
+>- - -
+
+###文档
+
+
+
+**1**.在命令行执行。
+
+```console
+code2doc D:\path\path
+```
+
+---
+
+**2**.或直接双击code2doc.exe
+
+```
+在console程序启动后，在输入或粘贴目标文件夹路径
+```
+
+
+---
+
+**3**.包含目标文件夹路径，生成的doc文件都将存入路径
+
+```console
+code2doc D:\src_path\path D:\target_path\path
+```
